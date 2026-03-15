91671b06c6a4aea3403db310c0e54c73cc4680da
Abhishek Mishra <abhishekmishra@Abhisheks-MacBook-Air.local>
2026-03-16 01:40:54 +0530

    Change web app port to 5002 to avoid conflicts

--- src/pyvault/web.py
+++ src/pyvault/web.py
@@ -106,4 +106,4 @@ def generate():
     return render_template('generate.html', password=password)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5002)
